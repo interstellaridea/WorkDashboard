a7d69c3d14b878debd9ab546d25b7860fee274af
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-05-17 13:12:09 -0700

    Implment JWT for zoom api v2 calls in a cronjob

--- cronjobs/zoomCrons.js
+++ cronjobs/zoomCrons.js
@@ -1,4 +1,7 @@
 const cronJob = require('node-cron');
+const jwt = require('jsonwebtoken');
+const axios = require('axios');
+const keys = require('../config/keys');
 
 module.exports = (app) => {
 
@@ -7,15 +10,26 @@ module.exports = (app) => {
     cronJob.schedule("*/10 * * * * 1-5", () => {
       console.log('every 10 sec');
 
-      // zoom api stuff
-      
-      // save / update to db 
-
-      
+      // Create payload for JWT to SHA256
+      const payload = {
+        iss: keys.ZOOM_API_KEY,
+        exp: ((new Date()).getTime() + 5000)
+      };
+      // Generate JWT, add token to headers of GET to zoom api v2 call
+      jwt.sign(
+        payload,
+        keys.ZOOM_API_SECRET, (err, token) => {
+          axios.get('https://api.zoom.us/v2/users', { headers: { Authorization: `Bearer ${token}`} })
+          .then(res => {
+            console.log(res.data)
+          })
+          .catch(err => {
+            console.log(err)
+          })        
+        }); 
     })
+
   } catch(err) {
     console.log('invalid pattern');
   }
-
-
 }
